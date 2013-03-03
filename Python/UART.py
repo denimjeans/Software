ccedd0220f7b422e64185e3c5019b106fca33372
Kampi <DanielKampert@kampis-elektroecke.de>
2013-03-03 02:15:05 +0100

    Updating Text

--- Python/UART.py
+++
@@ -1,8 +0,0 @@
-import serial
-import sys
-
-UART = serial.Serial("/dev/ttyAMA0", 9600)
-UART.open()
-UART.write("Anzahl an Argumente:" + str(len(sys.argv)) + "\n\r")
-UART.write("Argumente:" + str(sys.argv) + "\n\r")
-UART.close()
